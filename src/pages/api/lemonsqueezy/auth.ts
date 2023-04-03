f98d128cbbef2b125e415db78d86ba30b24de7e3
leon-do <leondo123@gmail.com>
2023-04-03 01:38:42 -0400

    mv endpoint

--- src/pages/api/lemonsqueezy/auth.ts
+++ src/pages/api/lemonsqueezy/auth.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
 import isAuthorized from "@/utils/isAuthorized";
 
 type Data = {
@@ -9,8 +8,7 @@ type Data = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   try {
     console.log("/lemonsqueezy/auth");
-    const { license_key } = req.body;
-    if (!(await isAuthorized(license_key))) return res.status(400).send({ success: false });
+    if (!(await isAuthorized(req.body.license_key))) return res.status(400).send({ success: false });
     res.status(200).send({ success: true });
   } catch (error) {
     console.error(error);
