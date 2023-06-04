daa2a15e39f6f80b678bcf9efe4051fb69f84493
leon-do <leondo123@gmail.com>
2023-06-03 20:23:43 -0400

    integromat/getViewFunctions

---
+++ src/pages/api/integromat/getViewFunctions.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  label: string;
+  value: string;
+};
+
+type Error = {
+  error: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data[] | Error>) {
+  try {
+    const abi = JSON.parse(req.query.abi as string);
+    const choices = abi
+      .filter((item: any) => item.type === "function" && item.stateMutability === "view")
+      .map((item: any) => ({
+        label: `${item.name}(${item.inputs.map((input: any) => `${input.type} ${input.name}`).join(",")})`,
+        value: item.name,
+      }));
+
+    res.status(200).send(choices);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ error: "Invalid ABI" });
+  }
+}
